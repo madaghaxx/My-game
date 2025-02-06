c800f23012950a441d6ccd3c9c8cd22b665b5a01
madaghaxx <aberhili0man@gmail.com>
2025-02-06 08:24:45 +0100

    test

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-let player = document.getElementById("player");
-let box = document.getElementsByClassName("the-all")[0];
-let boxPos = box.getBoundingClientRect();
+const player = document.getElementById("player");
+const box = document.getElementsByClassName("the-all")[0];
+const boxPos = box.getBoundingClientRect();
+let playerPos = player.getBoundingClientRect();
+
 if (player) {
   player.style.top = boxPos.height - player.offsetHeight - 1 + "px";
-  player.style.left = player.style.left || "0px";
-  addEventListener("keydown", (e) => {
+  player.style.left = boxPos.left + "px";
+  addEventListener("keydown", async (e) => {
+    playerPos = player.getBoundingClientRect();
     let left = parseInt(player.style.left) || 0;
     if (e.key == "ArrowRight" || e.key == "d") {
-      if (
-        !(parseInt(player.style.left) + player.offsetWidth >= boxPos.right - 10)
-      ) {
+      if (playerPos.right < boxPos.right) {
         player.style.left = left + 10 + "px";
       }
     }
     if (e.key == "ArrowLeft" || e.key == "a") {
-      if (!(parseInt(player.style.left) <= boxPos.left + 10)) {
+      if (playerPos.left > boxPos.left) {
         player.style.left = left - 10 + "px";
       }
     }
+    if (e.key == " ") {
+      const shot = document.createElement("div");
+      shot.classList.add("bullet");
+      shot.style.left = playerPos.left + playerPos.width / 2 + "px";
+      shot.style.top = playerPos.top + "px";
+      box.append(shot);
+      await shotmove(shot);
+    }
   });
 }
+
+const shotmove = async (shot) => {
+  const interval = setInterval(() => {
+    let shotPos = shot.getBoundingClientRect();
+    if (shotPos.top > 0) {
+      shot.style.top = shot.offsetTop - 100 + "px";
+    } else {
+      clearInterval(interval);
+      shot.remove();
+    }
+  }, 100);
+};
